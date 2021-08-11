546557b8def0025df655cf1e680af98599e00d4d
RashiParmar <rashinp01@gmail.com>
2021-08-11 16:12:36 +0530

    Account angular

---
+++ src/app/account.ts
@@ -0,0 +1,10 @@
+export class Account {
+    accountid?:number
+    applicationid?:string
+    ifsccode?:string
+    loanid?:string
+    accountpassword?:string
+    accountbalance?:number
+
+   
+}
